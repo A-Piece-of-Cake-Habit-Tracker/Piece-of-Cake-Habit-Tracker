b30794c2898a766db36ee1143f558823e96b3055
geilahtin <gttabanao@up.edu.ph>
2022-03-02 13:47:07 +0800

    Added Buttons

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+const App = () => (
+  <SafeAreaView style={styles.container}>
+    <View>
+      <Text style={styles.title}>
+        A Piece of Cake: Habit Tracker
+      </Text>
+      <Button
+        title="Main Menu"
+        onPress={() => Alert.alert('Main Menu Tab goes here')}
+      />
     </View>
-  );
-}
+    <View>
+      <Button
+        title="Statistics"
+        color="#f194ff"
+        onPress={() => Alert.alert('Statistics Tab goes here')}
+      />
+    </View>
+    <View>
+      <Button
+        title="Add Habit"
+        color="#3ded97"
+        onPress={() => Alert.alert('Add Habit Tab goes here')}
+      />
+    </View>
+    <View>
+      <Button
+        title="Edit Habit"
+        color="#f9e076"
+        onPress={() => Alert.alert('Edit Habit Tab goes here')}
+      />
+    </View>
+  </SafeAreaView>
+);
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    marginHorizontal: 16,
+  },
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
   },
 });
+
+export default App;

10a05c620fb3060e480981054736ed62393bf210
nazumi-1p <91103694+nazumi-1p@users.noreply.github.com>
2022-03-02 17:08:44 +0800

    Initial top bottom layout
    :)

--- App.js
+++ App.js
@@ -1,36 +1,41 @@
 import React from 'react';
-import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
+import { StyleSheet, Button, View, SafeAreaView, Text, Alert, TouchableOpacity, Image} from 'react-native';
 
 const App = () => (
   <SafeAreaView style={styles.container}>
-    <View>
-      <Text style={styles.title}>
-        A Piece of Cake: Habit Tracker
-      </Text>
+    {/* TOP AREA: Welcome, and edit and add buttons */}
+    <View style={styles.top}>
+    <Text style={styles.title}>
+        Welcome, Ketsib
+    </Text>
+    <View style={styles.topbuttons}>
+    <Button
+        title="Edit"
+        color="#4169E1"
+        onPress={() => Alert.alert('Edit Tab Goes Here')}
+      />
+    <TouchableOpacity 
+        style={{height:40}}
+        onPress={() => Alert.alert('Add habit tab goes here')}>
+        <Image source={require('./plus.png')}  style={styles.img}/>
+      </TouchableOpacity>
+    </View>
+    </View>
+
+    {/* MIDDLE AREA: Habit menu */}
+    <View style={styles.menu}>
+    </View>
+
+    {/* BOTTOM AREA: Menu and stats tabs buttons */}
+    <View style={styles.bottom}>
       <Button
         title="Main Menu"
         onPress={() => Alert.alert('Main Menu Tab goes here')}
       />
-    </View>
-    <View>
       <Button
         title="Statistics"
         color="#f194ff"
         onPress={() => Alert.alert('Statistics Tab goes here')}
-      />
-    </View>
-    <View>
-      <Button
-        title="Add Habit"
-        color="#3ded97"
-        onPress={() => Alert.alert('Add Habit Tab goes here')}
-      />
-    </View>
-    <View>
-      <Button
-        title="Edit Habit"
-        color="#f9e076"
-        onPress={() => Alert.alert('Edit Habit Tab goes here')}
       />
     </View>
   </SafeAreaView>
@@ -42,11 +47,28 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     marginHorizontal: 16,
+    marginVertical: 36
   },
   title: {
-    textAlign: 'center',
+    textAlign: 'left',
     marginVertical: 8,
   },
+  top:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  topbuttons:{
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  menu:{
+    flex:1,
+    justifyContent: 'center',
+  },
+  bottom: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  }
 });
 
 export default App;

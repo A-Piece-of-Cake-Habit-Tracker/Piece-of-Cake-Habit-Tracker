c53169e8c643fccefa5eaca211ec25ba702bff4f
Apolline Estrella <jdestrella1@up.edu.ph>
2022-03-02 18:40:34 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+<<<<<<< Updated upstream
 import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
 
 const App = () => (
@@ -35,11 +36,63 @@ const App = () => (
     </View>
   </SafeAreaView>
 );
+=======
+import { StyleSheet, StatusBar, Button, View, SafeAreaView, Text, Alert, TouchableOpacity, Image} from 'react-native';
 
+export default function App() {
+  return (
+    // This is the whole app container.
+    <SafeAreaView style={styles.container}>
+      {/* TOP AREA: Welcome, and edit and add buttons */}
+
+      {/* TOP AREA: This is one row. */}
+      <View style={styles.topContainer}>
+        <Text style={styles.title}>Hey there,{'\n'}Ketsib</Text>
+
+        <View style={styles.topbuttons}>
+        <Button
+            title="Edit"
+            color="#4169E1"
+            onPress={() => Alert.alert('Edit Tab Goes Here')}
+          />
+        </View>
+
+        <View style={styles.topbuttons}>
+        <TouchableOpacity 
+            style={{height:40}}
+            onPress={() => Alert.alert('Add habit tab goes here')}>
+            <Image source={require('./plus.png')}  style={styles.img}/>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      {/* MIDDLE AREA: Habit menu */}
+      <View style={styles.menu}>
+      </View>
+
+      {/* BOTTOM AREA: This is the bottom row. */}
+      <View style={styles.bottom}>
+        <Button
+        title="Main Menu"
+        onPress={() => Alert.alert('Main Menu Tab goes here')}
+        />
+        <Button
+        title="Statistics"
+        color="#f194ff"
+        onPress={() => Alert.alert('Statistics Tab goes here')}
+        />
+      </View>
+>>>>>>> Stashed changes
+
+    </SafeAreaView>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: 'column', // main axis
     flex: 1,
+<<<<<<< Updated upstream
     justifyContent: 'center',
     marginHorizontal: 16,
   },
@@ -50,3 +103,42 @@ const styles = StyleSheet.create({
 });
 
 export default App;
+=======
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight + 10: 0,
+    backgroundColor: '#eee',
+  },
+  topContainer: {
+    flexDirection: 'row', // main axis
+    justifyContent: 'space-around', // along main axis
+    alignItems: 'flex-start', // along cross axis
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight + 10: 0,
+    backgroundColor: '#aaa',
+  },
+  title: {
+    flex: 3,
+    textAlign: 'left',
+    color: "black",
+    fontSize: 45,
+    paddingLeft: 20,
+    paddingRight: 20,
+    lineHeight: 45,
+  },
+  top:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  topbuttons:{
+    flex: 1
+  },
+  menu:{
+    flex:1,
+    justifyContent: 'center',
+  },
+  bottom: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    bottom: 40,
+    backgroundColor: '#aaa'
+  }
+});
+>>>>>>> Stashed changes

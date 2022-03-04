f60ab88b312befa9d58891a0aee22892e4e99743
Apolline Estrella <jdestrella1@up.edu.ph>
2022-03-04 15:08:21 +0800

    Practiced Native Base

--- App.js
+++ App.js
@@ -1,96 +1,64 @@
 import React from 'react';
-<<<<<<< Updated upstream
-import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
-
-const App = () => (
-  <SafeAreaView style={styles.container}>
-    <View>
-      <Text style={styles.title}>
-        A Piece of Cake: Habit Tracker
-      </Text>
-      <Button
-=======
-import { StyleSheet, StatusBar, Button, View, SafeAreaView, Text, Alert, TouchableOpacity, Image} from 'react-native';
-import { NativeBaseProvider, Box } from "native-base";
+import { StyleSheet, StatusBar, Image} from 'react-native';
+import { NativeBaseProvider, Text, Box, Button } from 'native-base';
 
 export default function App() {
   return (
-    // This is the whole app container.
-    <SafeAreaView style={styles.container}>
-      {/* TOP AREA: Welcome, and edit and add buttons */}
+    <NativeBaseProvider>
+      <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
+        <Text>Open up App.js to start working on your app!</Text>
+      </Box>
 
-      {/* TOP AREA: This is one row. */}
-      <View style={styles.topContainer}>
-        <Text style={styles.title}>Hey there,{'\n'}Ketsib</Text>
+      <Box alignItems="center" style={styles.button}>
+        <Button onPress={() => console.log("hello world")}>Click Me</Button>
+      </Box>
 
-        <View style={styles.topbuttons}>
-        <Button
-            title="Edit"
-            color="#4169E1"
-            onPress={() => Alert.alert('Edit Tab Goes Here')}
-          />
-        </View>
-
-        <View style={styles.topbuttons}>
-        <TouchableOpacity 
-            style={{height:40}}
-            onPress={() => Alert.alert('Add habit tab goes here')}>
-            <Image source={require('./assets/plus.png')}  style={styles.img}/>
-          </TouchableOpacity>
-        </View>
-      </View>
-
-      {/* <NativeBaseProvider>
-        <Box>Hello world</Box>
-      </NativeBaseProvider> */}
-
-      {/* MIDDLE AREA: Habit menu */}
-      <View style={styles.menu}>
-      </View>
-
-      {/* BOTTOM AREA: This is the bottom row. */}
-      <View style={styles.bottom}>
-        <Button
->>>>>>> Stashed changes
-        title="Main Menu"
-        onPress={() => Alert.alert('Main Menu Tab goes here')}
-      />
-    </View>
-    <View>
-      <Button
-        title="Statistics"
-        color="#f194ff"
-        onPress={() => Alert.alert('Statistics Tab goes here')}
-      />
-    </View>
-    <View>
-      <Button
-        title="Add Habit"
-        color="#3ded97"
-        onPress={() => Alert.alert('Add Habit Tab goes here')}
-      />
-    </View>
-    <View>
-      <Button
-        title="Edit Habit"
-        color="#f9e076"
-        onPress={() => Alert.alert('Edit Habit Tab goes here')}
-      />
-    </View>
-  </SafeAreaView>
-);
-
+    </NativeBaseProvider>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: 'column', // main axis
     flex: 1,
-    justifyContent: 'center',
-    marginHorizontal: 16,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight + 10: 0,
+    backgroundColor: '#eee',
+  },
+  topContainer: {
+    flexDirection: 'row', // main axis
+    justifyContent: 'space-around', // along main axis
+    alignItems: 'flex-start', // along cross axis
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight + 10: 0,
+    backgroundColor: '#aaa',
   },
   title: {
-    textAlign: 'center',
-    marginVertical: 8,
+    flex: 3,
+    textAlign: 'left',
+    color: "black",
+    fontSize: 45,
+    paddingLeft: 20,
+    paddingRight: 20,
+    lineHeight: 45,
   },
+  top:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  topbuttons: {
+    flex: 1
+  },
+  img: {
+    width:35,
+    height:35
+  },
+  menu:{
+    flex:1,
+    justifyContent: 'center',
+  },
+  bottom: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    bottom: 40,
+    backgroundColor: '#aaa'
+  }
 });
-
-export default App;
